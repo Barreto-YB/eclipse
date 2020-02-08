1ddad6b352dc08583359b806094ec6a20afbe05f
Barreto <60776753+Barreto-YB@users.noreply.github.com>
2020-02-08 11:43:18 +0000

    adicionei arquivo via upload

---
+++ Hello GitHub/src/stp/com/barreto/git/HelloGit.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package stp.com.barreto.git;
+
+/**
+ * @author Yuri Barreto
+ *
+ */
+public class HelloGit {
+
+	/**
+	 * Examplo do git hub com eclipse
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello GitHub...");
+	}
+
+}
